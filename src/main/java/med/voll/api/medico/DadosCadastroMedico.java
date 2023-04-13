e7bf8622c9d58b6dbe9bd5bd96f1e0a133976ca1
Rafhaella <rafhaellamoreno@gmail.com>
2023-04-13 12:27:27 -0300

    First Commit

---
+++ src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -0,0 +1,32 @@
+package med.voll.api.medico;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import med.voll.api.endereco.DadosEndereco;
+
+public record DadosCadastroMedico(
+
+        /*@NotBlank que indica que um atributo do tipo String não pode ser nulo e nem vazio.
+        * @Email faz a validação de um endereço de e-mail
+        * Com o @Pattern é possível especificar uma expressão regular, ou seja, definir o tamanho de caracteres que um campo pode receber
+        * */
+        @NotBlank
+        String nome,
+        @NotBlank
+        @Email
+        String email,
+
+        @NotBlank
+        String telefone,
+        @NotBlank
+        @Pattern(regexp = "\\d{4,6}")
+        String crm,
+        @NotNull
+        Especialidade especialidade,
+        @NotNull
+        @Valid
+        DadosEndereco endereco) {
+}
